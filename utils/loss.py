593cf9658cf6209137375054cd2a6c8a820d344f
Tom <27803813+TDay1@users.noreply.github.com>
2022-07-31 12:14:43 +1000

    fixed bug where lambda_coord was only being applied on the x axis (#3)

--- utils/loss.py
+++ utils/loss.py
@@ -21,14 +21,12 @@ class Yoloss(torch.nn.Module):
         conf_loss = 0
         bbox_loss = 0
 
-
         # BBOX predicition
         pred_conf = y_pred[:, :, :, :, 4].flatten()
         truth_conf = y_true[:, :, :, :, 4].flatten()
         # sigma (c - c)^2
         conf_loss += torch.square( pred_conf - truth_conf ).sum()
         
-
         # BBOX loss
         pred_x = y_pred[:, :, :, :, 0].flatten()
         truth_x = y_true[:, :, :, :, 0].flatten()
@@ -46,10 +44,7 @@ class Yoloss(torch.nn.Module):
         truth_h = y_true[:, :, :, :, 3].flatten()
 
         # sigma ((w-w)^2 + (h-h)^2)
-        bbox_loss += self.lambda_coord * torch.square( pred_w - truth_w ).sum() + torch.square( pred_h - truth_h ).sum()
-
-
-        #print(f'bbox: {bbox_loss} conf:{conf_loss}')
+        bbox_loss += self.lambda_coord * ( torch.square( pred_w - truth_w ).sum() + torch.square( pred_h - truth_h ).sum() )
 
         return conf_loss + bbox_loss
 
